da32b8cca81ac843a3a6f3e47de6afe40f44206a
Chung Man Chau <chungman1993@gmail.com>
2016-10-17 10:30:48 +0100

    Delete main.js

--- now-tv-mockup/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.animate').not('.load').each(function(i) {
-        var $this = $(this);
-        var ind = i * 120;
-        setTimeout(function() {
-            $this.addClass('load');
-        }, ind);
-    });
-});
